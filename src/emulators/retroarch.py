02e34bce8360a74a82b6df354775e0d9568dfb7b
Isaiah Boone Bessire <153872567+Samson81@users.noreply.github.com>
2025-05-27 19:22:44 -0700

    Another test

--- src/emulators/retroarch.py
+++ src/emulators/retroarch.py
@@ -1,13 +1,4 @@
-import os
-import sys
-
-# Get the absolute path of the 'src' directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-if src_dir not in sys.path:
-    sys.path.insert(0, src_dir)
-
-from emu import Emulator
+from .emu import Emulator
  
 
 class RetroArch(Emulator):
